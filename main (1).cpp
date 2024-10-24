e167a9d73af75c4f2ce4dffff760a185fa296bd0
JulianDHC <juliandhc1402@gmail.com>
2024-10-24 17:04:47 -0500

    Cambios en clase

--- main (1).cpp
+++ main (1).cpp
@@ -7,6 +7,14 @@
 
 using namespace std;
 
+map<int, string> colores_jugadores = 
+{
+    {0, "\033[43m"},  // Amarillo
+    {1, "\033[41m"},  // Rojo
+    {2, "\033[44m"},  // Azul
+    {3, "\033[42m"}   // Verde
+};
+
 class jugadores
 {
 public:
@@ -63,48 +71,64 @@ public:
     }
     void mostrar()
     {
-        cout << c_[0] << c_[1] << c_[2] << c_[3] << "|";
+        for (int i = 0; i < 4; i++)
+        {
+            if (c_[i] == '.')
+            {
+                cout << c_[i];  // Sin color para los puntos
+            }
+            else
+            {
+                cout << colores_jugadores[i] << c_[i] << "\033[0m";  // Color según el jugador
+            }
+        }
+        cout << "|";
     }
 };
 
-class tablero
-{
+class tablero {
 private:
     int guia[11] = {3, 5, 7, 9, 11, 13, 11, 9, 7, 5, 3};
     string rotulo[11] = {"2 : ", "3 : ", "4 : ", "5 : ", "6 : ", "7 : ", "8 : ", "9 : ", "10: ", "11: ", "12: "};
     celda **ptr;
 
 public:
-    tablero()
+    tablero() 
     {
         ptr = (celda **)malloc(11 * 8);
-        for (int j = 0; j < 11; j++)
+        for (int j = 0; j < 11; j++) 
         {
-            ptr[j] = (celda *)malloc(guia[j]);
-        }
-
-        for (int i = 0; i < 11; i++)
-        {
-            for (int j = 0; j < guia[i]; j++)
+            ptr[j] = (celda *)malloc(guia[j] * sizeof(celda));
+            for (int i = 0; i < guia[j]; i++) 
             {
-                ptr[i][j] = celda('1', '2', '3', '4');
+                ptr[j][i] = celda('.', '.', '.', '.');
             }
         }
     }
-    void mostrar()
-    {
-        cout << "                                 TABLERO" << endl;
 
-        for (int i = 0; i < 11; i++)
+    void colocar_ficha(int suma, int jugador) 
+    {
+        int fila = suma - 2;
+        
+        for (int i = 0; i < guia[fila]; i++) 
         {
+            if (ptr[fila][i].c_[jugador] == '.') 
+            {  
+                ptr[fila][i].c_[jugador] = ' ';
+                break;
+            }
+        }
+    }
+
+    void mostrar() {
+        cout << "                                 TABLERO" << endl;
+        for (int i = 0; i < 11; i++) {
             cout << rotulo[i];
-            for (int k = 0; k < ((13 - guia[i]) / 2); k++)
-            {
+            for (int k = 0; k < ((13 - guia[i]) / 2); k++) {
                 cout << "     ";
             }
             cout << "|";
-            for (int j = 0; j < guia[i]; j++)
-            {
+            for (int j = 0; j < guia[i]; j++) {
                 ptr[i][j].mostrar();
             }
             cout << endl;
@@ -145,68 +169,73 @@ public:
 
 //00000000000000000000000000000000000000000000000000000000000000
 
-class juego
-{
+class juego {
 public:
     jugadores objeto_jugadores;
     Dado objeto_dado;            
-    int turno; 
+    tablero tablero_juego;
+    int turno2 = 0;
+    bool turno;
+    char condicion;
+    
+    juego() : turno2(0) {}
 
-    juego()
+    void turnos() {
+    int eleccion1, eleccion2;
+    int suma1, suma2;
+    
+    while (true)
     {
-        turno = 0;
-    }
+        suma2 = 0;
+        objeto_dado.lanzar();
+        cout << "Es el turno de " << objeto_jugadores.nombres[turno2] << endl;
+        cout << "Ha lanzado los dados y ha sacado: " << "\nDado 1: " << objeto_dado.resultados[0] 
+             << "\nDado 2: " << objeto_dado.resultados[1] << "\nDado 3: " << objeto_dado.resultados[2] 
+             << "\nDado 4: " << objeto_dado.resultados[3] << endl;
+        cout << "Elija un dado a emparejar (1-4): ";
+        cin >> eleccion1;
+        cout << "Elija el otro (1-4): ";
+        cin >> eleccion2;
+        eleccion1--;
+        eleccion2--;
+        
+        suma1 = objeto_dado.resultados[eleccion1] + objeto_dado.resultados[eleccion2];
+        for (int j = 0; j < 4; j++) 
+        {
+            if(j != eleccion1 && j != eleccion2) 
+            {
+                suma2 += objeto_dado.resultados[j];
+            }
+        }
 
-    void iniciar_juego()
+        tablero_juego.colocar_ficha(suma1, turno2);
+        tablero_juego.colocar_ficha(suma2, turno2);
+        tablero_juego.mostrar();
+        
+        cout << "¿Desea continuar?" << endl;
+        cin >> condicion;
+        if (condicion == 'n' || condicion == 'N')
+        {
+            turno2 = (turno2 + 1) % objeto_jugadores.Jugadores;
+        }
+    }
+}
+
+
+    void iniciar_juego() 
     {
         objeto_jugadores.PedirJugadores();
         objeto_jugadores.name_player();
-        
+        tablero_juego.mostrar();
         turnos();
     }
-    
-    void turnos()
-    {
-        int eleccion1;
-        int eleccion2;
-        int suma1;
-        int suma2;
-        
-        for (int i = 0; i < 5; i++)
-        {
-            suma2 = 0;
-            objeto_dado.lanzar();
-            cout << "Es el turno de " << objeto_jugadores.nombres[turno] << endl;
-            cout << "Ha lanzado los dados y ha sacado: " << "\nDado 1: " << objeto_dado.resultados[0] << "\nDado 2: " << objeto_dado.resultados[1] << "\nDado 3: " << objeto_dado.resultados[2] << "\nDado 4: " << objeto_dado.resultados[3] << endl;
-            cout << "Elija un dado a emparejar: ";
-            cin >> eleccion1;
-            cout << "Elija el otro: ";
-            cin >> eleccion2;
-            eleccion1 = eleccion1 - 1;
-            eleccion2 = eleccion2 - 1;
-            suma1 = objeto_dado.resultados[eleccion1] + objeto_dado.resultados[eleccion2];
-            for (int i = 0; i < 4; i++)
-            {
-                if(eleccion1 != i && eleccion2 != i)
-                {
-                    suma2 = suma2 + objeto_dado.resultados[i];
-                }
-            }
-            turno = (turno + 1) % objeto_jugadores.Jugadores;
-            cout << suma1 << " + " << suma2 << " -> Solo probando sumas" << endl;
-        }
-    };
 };
 
 //00000000000000000000000000000000000000000000000000000000000000
 
 int main()
 {
-    tablero t;
-    Dado d;
     juego game;
-
     game.iniciar_juego();  
-    t.mostrar();
     return 0;
 }
